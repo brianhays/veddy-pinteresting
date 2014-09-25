5973880767bbc29384b1a93f3d4901bd1fa41578
Brian Hays <brian.hays@gmail.com>
2014-09-25 11:58:36 -0400

    implement devise and simple_form

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'coffee-rails', '~> 4.0.0'
 # gem 'therubyracer',  platforms: :ruby
 
 gem 'bootstrap-sass'
+gem 'devise'
+gem 'simple_form'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
+  config.action_mailer.default_url_options = { :host => 'http://veddy-pinteresting.herokuapp.com/' }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
